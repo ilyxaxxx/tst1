4ff024ded6364cea50546a9722b904b15be220c8
Ilya Krylov <ilyxa.xxx@gmail.com>
2021-01-29 14:14:26 +0400

    changed to nexus

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -73,7 +73,7 @@ object Build : BuildType({
             commandType = build {
                 source = content {
                     content = """
-                        FROM openjdk
+                        FROM 100.100.100.101:1186/openjdk
                         ADD http://100.100.100.101:8081/repository/maven-releases/org/springframework/gs-maven/0.1.0/gs-maven-0.1.0.jar app.jar 
                         ENTRYPOINT ["java","-jar","app.jar"]
                     """.trimIndent()
